ed7def63cfd96b47b25b7f55cc5ccf7414d39dd3
Andre Dias <andreluizdiascustodio@gmail.com>
2023-03-09 10:15:50 -0300

    Add basic structure to main component

--- src/screens/coffeeInformation/coffee-information.js
+++ src/screens/coffeeInformation/coffee-information.js
@@ -2,7 +2,17 @@ import React from 'react'
 import * as S from './coffee-information.style'
 
 const CoffeeInformation = () => {
-  return <S.Wrapper>abc</S.Wrapper>
+  return (
+    <S.Wrapper>
+      <S.Title>Hora do café!</S.Title>
+      {/*  TODO adicionar toggle para fazer a partir de ml ou gramas de pó */}
+      <S.SubTitle>Quantos mls de café? ☕️</S.SubTitle>
+      <S.Input />
+      <S.SubTitle>Qual a torra?</S.SubTitle>
+      <S.Select />
+      <S.Button>Vamos lá!</S.Button>
+    </S.Wrapper>
+  )
 }
 
 export default CoffeeInformation
--- src/screens/coffeeInformation/coffee-information.style.js
+++ src/screens/coffeeInformation/coffee-information.style.js
@@ -1,9 +1,16 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
 
 export const Title = styled.h1``
 
+export const SubTitle = styled.h2``
+
 export const Input = styled.input``
+
+export const Select = styled.select``
 
 export const Button = styled.button``
